b4dd2a40039b7d353b7fc5c8a81322d515945b7d
wuzhi1234 <gaojingbao0302@gmail.com>
2015-07-27 12:37:47 +0800

    my solution to reverse integer ,take max_value into account

---
+++ Leetcode/src/M04_ReverseInteger.java
@@ -0,0 +1,28 @@
+/**
+ * Created by wuzhi1234 on 15/7/27.
+ */
+public class M04_ReverseInteger {
+    public static int reverse(int x){
+        if (x==0){
+            return 0;
+        }
+        if (x%10==0){
+            return reverse(x/10);
+        }
+        long tmp=0;
+        while (x/10!=0){
+            tmp=x%10+tmp*10;
+            x=x/10;
+        }
+        tmp=x%10+tmp*10;
+        if (tmp>Integer.MAX_VALUE || tmp<Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)tmp;
+    }
+
+    public static void main(String[] args) {
+        int x=reverse(1534236469);
+        System.out.println(x);
+    }
+}
