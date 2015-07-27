11c27e05ba686274ce0acb57d85d062b6c9b16b5
wuzhi1234 <gaojingbao0302@gmail.com>
2015-07-27 15:34:20 +0800

    删除仓库中的idea之前的准备工作

--- LeetcodeExample/src/P1_P50/P05_best_time_to_buy_and_sell_stock_II.java
+++ LeetcodeExample/src/P1_P50/P05_best_time_to_buy_and_sell_stock_II.java
@@ -1,22 +1,27 @@
 package P1_P50;
 
+import java.text.*;
+import java.util.*;
+
 public class P05_best_time_to_buy_and_sell_stock_II {
 
     public int maxProfit(int[] prices) {
-        
-        if (prices.length <=1){
+
+        if (prices.length <= 1) {
             return 0;
         }
-        int result=0;
-        for(int i=0;i<prices.length-1;i++){
-            if(prices[i]<prices[i+1]){
-                result+= prices[i+1]-prices[i];
+        int result = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                result += prices[i + 1] - prices[i];
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-
+        String str = "Sep 17, 2013";
+        Date date = new SimpleDateFormat("MMMM d, yy", Locale.ENGLISH).parse(str);
+        System.out.println(date);
     }
 }
