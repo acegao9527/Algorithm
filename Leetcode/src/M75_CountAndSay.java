9bd5c30a6f0f45c8df153a18a0680b63b193e61d
wuzhi1234 <gaojingbao0302@gmail.com>
2015-07-28 17:37:50 +0800

    添加CountAndSay的重写代码

--- Leetcode/src/M75_CountAndSay.java
+++ Leetcode/src/M75_CountAndSay.java
@@ -12,4 +12,35 @@ Given an integer n, generate the nth sequence.
 Note: The sequence of integers will be represented as a string.
 */
 public class M75_CountAndSay {
+    public String oneString(String s){
+        char tmp = s.charAt(0);
+        int num = 1;
+        StringBuilder newString = new StringBuilder("");
+        for (int i = 1; i < s.length(); i++) {
+            if (tmp==s.charAt(i)){
+                num++;
+                continue;
+            }
+            newString.append(Integer.toString(num)).append(tmp);
+            num = 1;
+            tmp = s.charAt(i);
+        }
+        newString.append(Integer.toString(num)).append(tmp);
+        return newString.toString();
+    }
+    public String CountAndSay(int n){
+        String result = "1";
+        int i = 1;
+        while (i < n){
+            result = oneString(result);
+            i++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        M75_CountAndSay m = new M75_CountAndSay();
+        String a = m.CountAndSay(5);
+        System.out.println(a);
+    }
 }
