c5d36081f3dcbb399690c229f0299f3503aeb49a
wuzhi1234 <gaojingbao0302@gmail.com>
2015-07-30 19:37:59 +0800

    代码注释，这个方法略显复杂

--- Leetcode/src/T87_AddBinary.java
+++ Leetcode/src/T87_AddBinary.java
@@ -12,11 +12,11 @@ public class T87_AddBinary {
     public static String addBinary(String a, String b) {
         int i = a.length() - 1;     // i指向a的末尾
         int j = b.length() - 1;     // j指向b的末尾
-        int c = 0;      // carry 进位
+        int c = 0;      // carry 进位，用来标注进位符
         // 先把String转为char数组便于处理
         char[] achar = a.toCharArray();
         char[] bchar = b.toCharArray();
-        // 结果数组
+        // 结果数组，长度为两个字符串中较长的+2
         char[] reschar = new char[Math.max(achar.length, bchar.length)+2];
         int k = 0;      // k指向结果数组的开头
 
@@ -24,7 +24,7 @@ public class T87_AddBinary {
             if(i<0 && j<0 && c==0){
                 break;
             }
-
+            //存储从低位到高位取出来的每位数
             int aint = 0;
             int bint = 0;
 
