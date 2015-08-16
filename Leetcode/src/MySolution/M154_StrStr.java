d39bab6add6326e23702ed5f6ebc713eb8b9fda9
wuzhi1234 <gaojingbao0302@gmail.com>
2015-08-16 15:46:09 +0800

    修改自己重写的StrStr的解法

--- Leetcode/src/MySolution/M154_StrStr.java
+++ Leetcode/src/MySolution/M154_StrStr.java
@@ -5,6 +5,10 @@ package MySolution;
  */
 public class M154_StrStr {
     public int strStr(String haystack,String needle){
+        if (needle==null) return 0;
+        if (haystack==null) return -1;
+        if (needle.length()>haystack.length()) return -1;
+
         for (int i = 0;  ; i++) {
             for (int j = 0; ; j++) {
                 if (j==needle.length()) return i;
