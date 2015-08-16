9efbe9d17b51653c4ae0d137aff95f00811bf38e
wuzhi1234 <gaojingbao0302@gmail.com>
2015-08-16 15:43:41 +0800

    添加自己重写的StrStr的解法

---
+++ Leetcode/src/MySolution/M154_StrStr.java
@@ -0,0 +1,21 @@
+package MySolution;
+
+/**
+ * Created by wuzhi1234 on 15/8/16.
+ */
+public class M154_StrStr {
+    public int strStr(String haystack,String needle){
+        for (int i = 0;  ; i++) {
+            for (int j = 0; ; j++) {
+                if (j==needle.length()) return i;
+                if (i+j==haystack.length()) return -1;
+                if (haystack.charAt(i+j)!=needle.charAt(j)) break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        M154_StrStr m = new M154_StrStr();
+        System.out.println(m.strStr("sfljsflj","lj"));
+    }
+}
