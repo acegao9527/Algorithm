dc091d3bd80cd73a78bc48acaf6580d5771e2ed9
wuzhi1234 <gaojingbao0302@gmail.com>
2015-08-24 16:42:27 +0800

    注释归并排序

--- AllSorts/src/MergeSort.java
+++ AllSorts/src/MergeSort.java
@@ -10,6 +10,7 @@ public class MergeSort {
      * 简介:将两个（或两个以上）有序表合并成一个新的有序表 即把待排序序列分为若干个子序列，每个子序列是有序的。然后再把有序子序列合并为整体有序序列
      * 时间复杂度为O(nlogn)
      * 稳定排序方式
+     *
      * @param nums 待排序数组
      * @return 输出有序数组
      */
@@ -27,6 +28,7 @@ public class MergeSort {
     }
 
     public static void merge(int[] nums, int low, int mid, int high) {
+        //申请空间
         int[] temp = new int[high - low + 1];
         int i = low;// 左指针
         int j = mid + 1;// 右指针
@@ -61,9 +63,9 @@ public class MergeSort {
     // 归并排序的实现
     public static void main(String[] args) {
 
-        int[] nums = { 2, 7, 8, 3, 1, 6, 9, 0, 5, 4 };
+        int[] nums = {2, 7, 8, 3, 1, 6, 9, 0, 5, 4};
 
-        MergeSort.sort(nums, 0, nums.length-1);
+        MergeSort.sort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
     }
 }
