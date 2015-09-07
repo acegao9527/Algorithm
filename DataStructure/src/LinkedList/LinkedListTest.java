deaa2692c4cd69bceb703e2811364f9e0df8de6a
wuzhi1234 <gaojingbao0302@gmail.com>
2015-09-07 14:37:55 +0800

    实现单链表的反转

--- DataStructure/src/LinkedList/LinkedListTest.java
+++ DataStructure/src/LinkedList/LinkedListTest.java
@@ -12,16 +12,23 @@ public class LinkedListTest {
             this.value = value;
         }
     }
-
+    /*从头到尾遍历原链表，每遍历一个结点，
+    将其摘下放在新链表的最前端。注意链表为空和只有一个结点的情况。时间复杂度为O（n）。
+    */
     public static LinkedNode ReverseList(LinkedNode node){
         if (node==null || node.next==null){
             return node;
         }
         LinkedNode result = null;
-        LinkedNode tmp = node;
-        while (tmp!=null){
-            result=node;
-            tmp=
+        LinkedNode current = node;
+        while (current!=null){
+            LinkedNode tmp =current;//存下当前节点
+            current=current.next;//当前节点指向下个节点
+            tmp.next=result;//将当前节点的next指向原来的结果头
+            result=tmp;
         }
+        return result;
     }
+
+
 }
